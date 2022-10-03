fc04c50e2d6a80ab0d1404cc0cadd607aef01bd6
esy <yesman5307@gmail.com>
2022-10-03 20:20:55 +0900

    finished first Clone code

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // 완전히 새롭게 생성한 객체
+            em.persist(item); // 신규 등록
+        } else { //기존에 있는 객체
+            em.merge(item); // 정보 갱신
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
